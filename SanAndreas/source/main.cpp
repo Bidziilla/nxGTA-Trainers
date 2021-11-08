e3e06728abb6bb7d505514bdf126512a2dd12f30
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-08 03:52:47 -0500

    Added main.cpp for San Andreas trainer
    Placeholder until the game releases.

---
+++ SanAndreas/source/main.cpp
@@ -0,0 +1,171 @@
+#define TESLA_INIT_IMPL // If you have more than one file using the tesla header, only define this in the main one
+#include <tesla.hpp>    // The Tesla Header
+#include <dmntcht.h>
+#include "CHEAT/DEFINITIONS/constants.h"
+#include "CHEAT/DEFINITIONS/addresses.h"
+#include "CHEAT/DEFINITIONS/noval.h"
+#include "CHEAT/cheatFunctions.h"
+//DmntCheatProcessMetadata metadata;
+
+#define GAME_TITLE_ID 0x0000000000000000
+static bool initialized=false;
+
+//CHECK
+bool bid_match() {
+    const unsigned char build_id_size=8;
+    const unsigned char expected_build_id[build_id_size]={0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    for(unsigned char i=0; i<build_id_size; i++)
+        if(metadata.main_nso_build_id[i]!=expected_build_id[i])
+            return false;
+    return true;
+}
+
+bool debugService_isRunning(){
+    Handle handle;
+    SmServiceName service=smEncodeName("dmnt:cht");
+    bool isRunning=R_FAILED(smRegisterService(&handle, service, false, 1));
+    svcCloseHandle(handle);
+    if(!isRunning) smUnregisterService(service);
+    return isRunning;
+}
+
+class LEGALGUI : public tsl::Gui {
+public:
+    LEGALGUI() {}
+
+    virtual tsl::elm::Element* createUI() override {
+        auto *rootFrame = new tsl::elm::OverlayFrame("Legal Information", "You should read this.");
+		auto list = new tsl::elm::List();
+		if (initialized&&debugService_isRunning()&&metadata.title_id==GAME_TITLE_ID&&bid_match())
+		{
+			list->addItem(new tsl::elm::CategoryHeader("THIS SOFTWARE MUST NOT BE SOLD NEITHER \nALONE NOR AS PART OF A BUNDLE. \n\nIF YOU PAID FOR THIS SOFTWARE, YOU HAVE \nBEEN SCAMMED AND SHOULD DEMAND YOUR \nMONEY BACK IMMEDIATELY.  \n\n\nThis software provides users with \nspecial cheats for one or more games in \nthe Grand Theft Auto Trilogy. \nThese cheats were made through \nthe use of cleanroom reverse engineering \ntherefore, no copyright law has been broken. \n\nIn other words, this is a passion project \nplease don't sue me."));
+		}
+	
+		rootFrame->setContent(list);
+        return rootFrame;
+    }
+};
+
+class GuiTest : public tsl::Gui {
+public:
+    GuiTest(u8 arg1, u8 arg2, bool arg3) {}
+
+    virtual tsl::elm::Element* createUI() override {
+        auto rootFrame = new tsl::elm::OverlayFrame("GTA: SA TRAINER", "Made by Bidziilla, Ver: 1.0");
+        auto list = new tsl::elm::List();
+        if (initialized&&debugService_isRunning()&&metadata.title_id==GAME_TITLE_ID&&bid_match())
+        {
+			auto *PlayerCheats = new tsl::elm::ListItem("Player Cheats", "\u2600");
+			PlayerCheats->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                
+                return true;
+            }
+
+            return false;
+			});
+			
+			auto *TimeCheats = new tsl::elm::ListItem("Time Cheats", "\u2600");
+			TimeCheats->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                
+                return true;
+            }
+
+            return false;
+			});
+			
+			auto *ShaderCheats = new tsl::elm::ListItem("Shader Cheats", "\u2600");
+			ShaderCheats->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                
+                return true;
+            }
+
+            return false;
+			});
+			
+			auto *PhysicsCheats = new tsl::elm::ListItem("Physics Cheats", "\u2600");
+			PhysicsCheats->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                
+                return true;
+            }
+
+            return false;
+			});
+			
+			auto *MiscCheats = new tsl::elm::ListItem("Misc Cheats", "\u2600");
+			MiscCheats->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                
+                return true;
+            }
+
+            return false;
+			});
+			
+			auto *LEGALINFORMATION = new tsl::elm::ListItem("Legal Information", "\u2600");
+			LEGALINFORMATION->setClickListener([](u64 keys) { 
+            if (keys & KEY_A) {
+                tsl::changeTo<LEGALGUI>();
+                return true;
+            }
+
+            return false;
+			});
+			
+			list->addItem(new tsl::elm::CategoryHeader("Cheats"));
+			list->addItem(PlayerCheats);
+			list->addItem(TimeCheats);
+			list->addItem(ShaderCheats);
+			list->addItem(PhysicsCheats);
+			list->addItem(MiscCheats);
+			list->addItem(new tsl::elm::CategoryHeader("Legal"));
+			list->addItem(LEGALINFORMATION);
+		}
+		/*else
+        {
+			list->addItem(new tsl::elm::CategoryHeader("All you had to do was load the right game, CJ! \nERROR: TitleID Mismatch"));
+        }*/
+
+        rootFrame->setContent(list);
+        return rootFrame;
+    }
+
+    virtual bool handleInput(u64 keysDown, u64 keysHeld, touchPosition touchInput, JoystickPosition leftJoyStick, JoystickPosition rightJoyStick) override {
+        return false;
+    }
+};
+
+class OverlayTest : public tsl::Overlay {
+public:
+                                             
+    virtual void initServices() override
+    {
+        if (debugService_isRunning())
+        {
+            dmntchtInitialize();
+            dmntchtForceOpenCheatProcess();
+            dmntchtGetCheatProcessMetadata(&metadata);
+
+            initialized=true;
+        }
+    }
+    virtual void exitServices() override
+    {
+        dmntchtExit();
+        initialized=false;
+    }
+
+    virtual void onShow() override {}
+    virtual void onHide() override {}
+
+    virtual std::unique_ptr<tsl::Gui> loadInitialGui() override {
+        return initially<GuiTest>(1, 2, true);
+    }
+};
+
+int main(int argc, char **argv) {
+    return tsl::loop<OverlayTest>(argc, argv);
+}
