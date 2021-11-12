b1855c472a796a11d329964bdd43557679cf584e
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-11 22:49:35 -0500

    Update TIMECHEAT.h
    Removed Freeze Timer code. No need for it if the code doesn't exist yet.

--- SanAndreas/source/CHEAT/CHEAT/TIMECHEAT.h
+++ SanAndreas/source/CHEAT/CHEAT/TIMECHEAT.h
@@ -9,12 +9,14 @@ bool TimeEditHour2(T *entry)
   { 
     if (keys & KEY_DRIGHT) 
 	{
-		//util::AddToOffset(1, ADDR_HOUR, 0x1);
+		//util::EditTime(0, ADDR_HOUR, 1, 0x17, 1);
+		util::AddToOffset(0, ADDR_HOUR, 0x1);
 		return true;
 	}
 	else if (keys & KEY_DLEFT) 
 	{
-		//util::SubFromOffset(1, ADDR_HOUR, 0x1);
+		//util::EditTime(1, ADDR_HOUR, 1, 1, 2);
+		util::SubFromOffset(0, ADDR_HOUR, 0x1);
 		return true;		
     }
 
@@ -30,35 +32,12 @@ bool TimeEditMin2(T *entry)
   { 
     if (keys & KEY_DRIGHT) 
 	{
-		//util::AddToOffset(1, ADDR_MIN, 0x1);
+		util::AddToOffset(0, ADDR_MIN, 0x1);
 		return true;
 	}
 	else if (keys & KEY_DLEFT) 
 	{
-		//util::SubFromOffset(1, ADDR_MIN, 0x1);
-		return true;		
-    }
-
-    return false;
-  });
-  return true;
-}
-
-template <typename T>
-bool TimeFreeze2(T *entry) 
-{
-  entry->setClickListener([](u64 keys) 
-  { 
-    if (keys & KEY_DRIGHT) 
-	{
-		//dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_TIMEFREEZE1, &PlaceHolder, sizeof(4));
-		//dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_TIMEFREEZE2, &PlaceHolder, sizeof(4));
-		return true;
-	}
-	else if (keys & KEY_DLEFT) 
-	{
-		//dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_TIMEFREEZE1, &PlaceHolder, sizeof(4));
-		//dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_TIMEFREEZE2, &PlaceHolder, sizeof(4));
+		util::SubFromOffset(0, ADDR_MIN, 0x1);
 		return true;		
     }
 
