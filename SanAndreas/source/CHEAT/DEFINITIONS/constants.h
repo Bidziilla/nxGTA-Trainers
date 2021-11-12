0fb30886f59a4780d6039753cb23c99ac07f4127
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-11 22:44:24 -0500

    Added values for cheats

--- SanAndreas/source/CHEAT/DEFINITIONS/constants.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/constants.h
@@ -3,6 +3,13 @@
 #include <string>
 
 uint32_t Zero = 0x00000000;
-uint32_t PlaceHolder = 0x00000000;
+
+//Register1
+uint32_t DefaultFloat1 = 0x1E2E1000;
+uint32_t AudioFix = 0x1E241000;
+
+//Register2
+uint32_t DefaultFloat2 = 0x1E2E1002;
+uint32_t CJRunFast = 0x1E231002;
 
 #endif // CONSTANTS_H
