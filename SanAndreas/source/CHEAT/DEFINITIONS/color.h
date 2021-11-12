32135275cdb74041603494ec8e4084ee45b1f504
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-11 22:45:29 -0500

    Adding colors for custom hud

---
+++ SanAndreas/source/CHEAT/DEFINITIONS/color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+#include <string>
+
+#define GSFGreen 0xFF01CB5C
+#define BallaPurple 0xFFC036F7
+#define VagosYellow 0xFF0DA3D1
+#define DarkBlue 0xFF61212
+
+#endif // COLOR_H
