d6ec0a5ed815b6f0c0c385925b0fc8e793b1f7f9
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-11 22:46:26 -0500

    Added vehicle list for carspawner

---
+++ SanAndreas/source/CHEAT/DEFINITIONS/VEHICLES.h
@@ -0,0 +1,12 @@
+#ifndef VEHICLES_H
+#define VEHICLES_H
+#include <string>
+
+uint32_t TrashMaster = 0x52803300;
+uint32_t Stretch = 0x52803320;
+uint32_t FBITruck = 0x52804200;
+uint32_t CombineHarvester = 0x52804280;
+uint32_t Andromada = 0x52804A00;
+uint32_t RCCAM = 0x52804A40;
+
+#endif // VEHICLES_H
