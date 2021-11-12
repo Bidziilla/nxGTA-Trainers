edd1dda8fdfa5ef01dc270e9e9b6d6630553e0ce
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-11 22:48:43 -0500

    Made stuff work
    Made money cheats work, added muscle editor.

--- SanAndreas/source/CHEAT/CHEAT/PLAYERCHEAT.h
+++ SanAndreas/source/CHEAT/CHEAT/PLAYERCHEAT.h
@@ -9,7 +9,7 @@ bool Add250k2(T *entry)
   { 
     if (keys & KEY_A) 
 	{
-		//util::AddToOffset(1, ADDR_CJMONEY, 0x3D090);
+		util::AddToOffset(0, ADDR_CJMONEY, 0x61A8);
 		return true;
     }
 
@@ -25,7 +25,8 @@ bool RemoveCash2(T *entry)
   { 
     if (keys & KEY_A) 
 	{
-		//dmntchtWriteCheatProcessMemory(metadata.heap_extents.base + ADDR_CJMONEY, &Zero, sizeof(4));
+		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_CJMONEY, &Zero, sizeof(4));
+		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_CJMONEYDISPLAY, &Zero, sizeof(4));
 		return true;
     }
 
@@ -92,6 +93,27 @@ bool MaxAmmo2(T *entry)
 	{
 		//dmntchtWriteCheatProcessMemory(metadata.heap_extents.base + ADDR_PLACEHOLDER, &PlaceHolder, sizeof(4));
 		return true;
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool MuscleEdit2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT)  
+	{
+		util::AddToOffset(0, ADDR_CJMUSCLE, 0x250000);
+		return true;
+    }
+	else if (keys & KEY_DLEFT) 
+	{
+		util::SubFromOffset(0, ADDR_CJMUSCLE, 0x250000);
+		return true;		
     }
 
     return false;
