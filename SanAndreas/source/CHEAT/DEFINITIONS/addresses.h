6f07747a40e8411743d0c8b9734f15e5f99bb8e2
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-12 02:41:55 -0500

    Added offsets for more stats.

--- SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
@@ -3,34 +3,36 @@
 #include <string>
 
 //PLAYER
-#define ADDR_CJHEALTH 0x0
-#define ADDR_CJARMOR 0x0
-#define ADDR_CJOXYGEN 0x0
+//#define ADDR_CJHEALTH 0x0
+//#define ADDR_CJARMOR 0x0
+//#define ADDR_CJOXYGEN 0x0
 #define ADDR_CJMONEY 0x69AFAD8              //MAIN
 #define ADDR_CJMONEYDISPLAY 0x69AFADC       //MAIN
 #define ADDR_CJRUNSPEED 0xE10160            //MAIN
 
 //STATS
-#define ADDR_CJFAT 0x0
-#define ADDR_CJSTAMINA 0x0
+#define ADDR_CJFAT 0x69AAE68                //MAIN
+#define ADDR_CJSTAMINA 0x69AAE6C            //MAIN
 #define ADDR_CJMUSCLE 0x69AAE70             //MAIN
-#define ADDR_CJSEXAPPEAL 0x0
+#define ADDR_CJMAXHEALTH 0x69AAE74          //MAIN
+#define ADDR_CJSEXAPPEAL 0x69AAE78          //MAIN
+#define ADDR_CJRESPECT 0x69AAF14            //MAIN
 
 //TIME
 #define ADDR_HOUR 0x6983264
-#define ADDR_MIN 0x6983265
+#define ADDR_MIN 0x6983268
 
 //GFX
-#define ADDR_DRAWDISTANCE 0x0
-#define ADDR_BRIGHTNESS 0x0
-#define ADDR_SATURATION 0x0
+//#define ADDR_DRAWDISTANCE 0x0
+//#define ADDR_BRIGHTNESS 0x0
+//#define ADDR_SATURATION 0x0
 
 //AUDIO
 #define ADDR_AUDIOVOL1 0x375A618            //MAIN  (SFX)
 //#define ADDR_AUDIOVOL2 0x375A5B8          //MAIN  (RADIO)
 
 //PHYSICS
-#define ADDR_GRAVITY 0x0
+//#define ADDR_GRAVITY 0x0
 
 //HUD
 #define ADDR_VEHTEXTRGB 0x69F4FF4           //MAIN
@@ -38,8 +40,8 @@
 
 //MISC
 #define ADDR_CHEATTRASHID 0xE93CA4          //MAIN (A, R, A, R, Left, Left, R, L, A, Right)
-#define ADDR_CHEATHYDRAID 0x0
-#define ADDR_CHEATHUNTERID 0x0
-#define ADDR_VEHICLEBOOST 0x0
+//#define ADDR_CHEATHYDRAID 0x0
+//#define ADDR_CHEATHUNTERID 0x0
+//#define ADDR_VEHICLEBOOST 0x0
 
 #endif // ADDRESSES_H
