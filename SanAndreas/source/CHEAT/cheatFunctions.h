e9794a2855446c28759208212bc7d02d4cb68c56
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-15 00:02:43 -0500

    Update cheatFunctions.h

--- SanAndreas/source/CHEAT/cheatFunctions.h
+++ SanAndreas/source/CHEAT/cheatFunctions.h
@@ -11,6 +11,7 @@ namespace util
 	u32 CopyHeapMemory(u32 read, u32 write);
 	u32 AddToOffset(u8 region, u32 Address, u32 Input);
 	u32 SubFromOffset(u8 region, u32 Address, u32 Input);
+	u32 ConvertToMOV(u32 Address, u8 Register, u16 Value);
 	//u32 EditTime(u8 Operation, u32 Address, u32 Input, u8 compare, u8 resetValue);
 	//u32 EnableCustomHud();
 }
