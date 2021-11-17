aac7a79a5e2509a96f1859cae993ab9ce2b5f489
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-17 18:14:47 -0500

    Added definitions for new functions.

--- ViceCity/source/CHEAT/cheatFunctions.h
+++ ViceCity/source/CHEAT/cheatFunctions.h
@@ -11,5 +11,7 @@ namespace util
 	u32 CopyHeapMemory(u32 read, u32 write);
 	u32 AddToOffset(u8 region, u32 Address, u32 Input);
 	u32 SubFromOffset(u8 region, u32 Address, u32 Input);
+	u32 ConvertToMOV(u32 Address, u8 Register, u16 Value);
+	u32 CheckIDRange(u16 Value, u16 min, u16 max, u32 offset, u16 reset);
 }
 #endif // CHEATFUNCTIONS_H
