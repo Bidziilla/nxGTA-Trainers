5fe51352e41033c71197be692f66c56013e95c00
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-17 18:21:17 -0500

    Added the rest of the code to the repo.

---
+++ ViceCity/source/CHEAT/CHEAT/VEHCHEAT.h
@@ -0,0 +1,59 @@
+#ifndef VEHCHEAT_H
+#define VEHCHEAT_H
+#include <string>
+
+template <typename T>
+bool CarIDPicker2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::AddToOffset(1, Addr_CarID, 0x1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::SubFromOffset(1, Addr_CarID, 0x1);
+		return true;
+	}
+	if (keys & KEY_R) 
+	{
+		util::AddToOffset(1, Addr_CarID, 0x10);
+		return true;
+	}
+	else if (keys & KEY_L) 
+	{
+		util::SubFromOffset(1, Addr_CarID, 0x10);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool SetCarID2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_A) 
+	{
+		dmntchtReadCheatProcessMemory(metadata.heap_extents.base + Addr_CarID, &ActualCarID, sizeof(2));
+		
+		for(int i = 0; i <= 8; i++)
+		{
+			util::CheckIDRange(ActualCarID, 0x81, 0xED, Addr_CarID, 0x82);
+			util::ConvertToMOV(ADDR_CHEATRHINOID, 0, ActualCarID);
+		}
+        
+		return true;
+	}
+
+    return false;
+  });
+  return true;
+}
+
+#endif // VEHCHEAT_H
