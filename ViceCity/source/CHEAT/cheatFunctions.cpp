c8df05c2ca0910dee24a27d4ad59c2715f751cc2
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-17 18:15:10 -0500

    Added new functions.

--- ViceCity/source/CHEAT/cheatFunctions.cpp
+++ ViceCity/source/CHEAT/cheatFunctions.cpp
@@ -1,6 +1,7 @@
 #include "cheatFunctions.h"
 
 DmntCheatProcessMetadata metadata;
+int Read16;
 int Read32;
 int MathOutput;
 
@@ -55,4 +56,36 @@ u32 util::SubFromOffset(u8 region, u32 Address, u32 Input)
 		dmntchtWriteCheatProcessMemory(metadata.heap_extents.base + Address, &MathOutput, sizeof(4));
 	}
 	return true;
+}
+
+u32 util::ConvertToMOV(u32 Address, u8 Register, u16 Value)
+{
+	uint32_t M0VV = 0x52800000 + Register;
+	uint32_t M0VVPlus = 0x52900000 + Register;
+    dmntchtGetCheatProcessMetadata(&metadata);
+	
+	if (Value <= 0x7FFF) 
+	{
+		uint16_t MathDo = (Value * 2) & 0xFFFF;
+		uint32_t Result = M0VV + (((MathDo << 4) & 0xFFFFF));
+		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + Address, &Result, sizeof(4));
+	}
+	else if (Value >= 0x8000)
+	{
+		uint16_t MathDo = (Value * 2) & 0xFFFF;
+		uint32_t Result = M0VVPlus + (((MathDo << 4) & 0xFFFFF));
+		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + Address, &Result, sizeof(4));
+	}
+	return true;
+}
+
+u32 util::CheckIDRange(u16 Value, u16 min, u16 max, u32 offset, u16 reset)
+{
+    dmntchtGetCheatProcessMetadata(&metadata);
+    
+    if (Value <= min || Value >= max) 
+    {
+        dmntchtWriteCheatProcessMemory(metadata.heap_extents.base + offset, &reset, sizeof(2));
+    }
+    return true;
 }
