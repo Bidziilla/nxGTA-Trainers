991c8c6c561f651eaed37c1562d55c04ef8ca4aa
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-26 22:47:59 -0500

    Update addresses.h
    Added new offsets for toggling cheats on and off, removed address for money display and minute offset because we technically don't need to define it anymore. THANKS MATH!!

--- SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
@@ -3,38 +3,36 @@
 #include <string>
 
 //PLAYER
-//#define ADDR_CJHEALTH 0x0
-//#define ADDR_CJARMOR 0x0
-//#define ADDR_CJOXYGEN 0x0
-#define ADDR_CJMONEY 0x69AFAD8              //MAIN
-#define ADDR_CJMONEYDISPLAY 0x69AFADC       //MAIN
-#define ADDR_CJRUNSPEED 0xE10160            //MAIN
+#define ADDR_CJMONEY 0x69B6B18              //MAIN
 
 //STATS
-#define ADDR_CJFAT 0x69AAE68                //MAIN
-#define ADDR_CJSTAMINA 0x69AAE6C            //MAIN
-#define ADDR_CJMUSCLE 0x69AAE70             //MAIN
-#define ADDR_CJMAXHEALTH 0x69AAE74          //MAIN
-#define ADDR_CJSEXAPPEAL 0x69AAE78          //MAIN
-#define ADDR_CJRESPECT 0x69AAF14            //MAIN
+#define ADDR_CJFAT 0x69B1EA8                //MAIN
+#define ADDR_CJSTAMINA 0x69B1EAC            //MAIN
+#define ADDR_CJMUSCLE 0x69B1EB0             //MAIN
+#define ADDR_CJMAXHEALTH 0x69B1EB4          //MAIN
+#define ADDR_CJRESPECT 0x69B1F54            //MAIN
 
 //TIME
-#define ADDR_HOUR 0x6983264
-#define ADDR_MIN 0x6983268
-
-//AUDIO
-#define ADDR_AUDIOVOL1 0x375A618            //MAIN  (SFX)
-//#define ADDR_AUDIOVOL2 0x375A5B8          //MAIN  (RADIO)
+#define ADDR_TIME 0x698A264                 //MAIN
 
 //PHYSICS
-//#define ADDR_GRAVITY 0x0
-
-//HUD
-#define ADDR_VEHTEXTRGB 0x69F4FF4           //MAIN
-#define ADDR_STREETTEXTRGB 0x69F4FFC        //MAIN
+#define ADDR_CJRUNSPEED 0xE13090            //MAIN
 
 //MISC
-#define ADDR_CHEATTRASHID 0xE93CA4          //MAIN (A, R, A, R, Left, Left, R, L, A, Right)
+#define ADDR_CHEATTRASHID 0xE96D84          //MAIN (A, R, A, R, Left, Left, R, L, A, Right)
 #define Addr_CarID 0x40
+
+//CHEATS     -   These are toggled as 0 = off, 1 = on. :3  All are in main
+#define ADDR_InsaneHandling 0x68D3F57
+#define ADDR_FlyingBoat 0x68D3F5E
+#define ADDR_CarsOnWater 0x68D3F5D
+#define ADDR_SportsCars 0x68D3F6B
+#define ADDR_BunnyHop 0x68D3F6D
+#define ADDR_FlyingCars 0x68D3F6C
+#define ADDR_DeathCar 0x68D3F70 
+#define ADDR_SuperJump 0x68D3F78            
+#define ADDR_InfBreath 0x68D3F7A
+#define ADDR_WantedLock 0x68D3F7D
+#define ADDR_SuperPunch 0x68D3F7F
 
 #endif // ADDRESSES_H
