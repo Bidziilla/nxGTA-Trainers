c8a9bc11517c7947824ac544a93e178aa37c0e3f
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-26 22:45:27 -0500

    Update constants.h
    Update this shit.

--- SanAndreas/source/CHEAT/DEFINITIONS/constants.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/constants.h
@@ -6,10 +6,9 @@ uint32_t Zero = 0x00000000;
 
 //Register1
 uint32_t DefaultFloat1 = 0x1E2E1000;
-uint32_t AudioFix = 0x1E241000;
 
 //Register2
 uint32_t DefaultFloat2 = 0x1E2E1002;
-uint32_t CJRunFast = 0x1E231002;
+uint32_t CJRunFast = 0x1E241002;
 
 #endif // CONSTANTS_H
