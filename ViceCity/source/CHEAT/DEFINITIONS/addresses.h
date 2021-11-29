ea9bdb94cb3ea99be19360ad3af0480fe4683f54
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-11-29 04:31:17 -0500

    Update addresses.h

--- ViceCity/source/CHEAT/DEFINITIONS/addresses.h
+++ ViceCity/source/CHEAT/DEFINITIONS/addresses.h
@@ -2,13 +2,11 @@
 #define ADDRESSES_H
 #include <string>
 
-#define ADDR_MONEY 0x6740F48
+#define ADDR_MONEY 0x6745008
 
-#define ADDR_HudMode 0x1D6D7BC
+#define ADDR_TIME 0x66AC358
 
-#define ADDR_TIME 0x66A8358
-
-#define ADDR_CHEATRHINOID 0xED2EA8          //MAIN (A, A, L, A, A, A, L, ZL, R, X, A, X)
+#define ADDR_CHEATRHINOID 0xED5FA8          //MAIN (A, A, L, A, A, A, L, ZL, R, X, A, X)
 #define Addr_CarID 0x40
 
 #endif // ADDRESSES_H
