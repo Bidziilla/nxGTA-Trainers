bc605e15452c332fc6481d488ad8de5b6f97d3fb
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-12-19 01:51:27 -0500

    Update constants.h
    Removed defined values, we will keep this here just for the future in case we need it again. (Mostly if a value has to be repeated over and over in different files.)

--- SanAndreas/source/CHEAT/DEFINITIONS/constants.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/constants.h
@@ -2,13 +2,4 @@
 #define CONSTANTS_H
 #include <string>
 
-uint32_t Zero = 0x00000000;
-
-//Register1
-uint32_t DefaultFloat1 = 0x1E2E1000;
-
-//Register2
-uint32_t DefaultFloat2 = 0x1E2E1002;
-uint32_t CJRunFast = 0x1E241002;
-
 #endif // CONSTANTS_H
