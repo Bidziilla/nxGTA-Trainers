0625686cd8b4c22a8ccac1917f201e6781c495d6
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-12-19 01:52:42 -0500

    Update MISCCHEAT.h
    Added rockstar approved cheats.

--- SanAndreas/source/CHEAT/CHEAT/MISCCHEAT.h
+++ SanAndreas/source/CHEAT/CHEAT/MISCCHEAT.h
@@ -3,18 +3,207 @@
 #include <string>
 
 template <typename T>
-bool FixAudioo2(T *entry) 
+bool FlyingBoat2(T *entry) 
 {
   entry->setClickListener([](u64 keys) 
   { 
     if (keys & KEY_DRIGHT) 
 	{
-		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_AUDIOVOL1, &AudioFix, sizeof(4));
+		util::Write8(0, ADDR_CheatBase, 1);
 		return true;
 	}
 	else if (keys & KEY_DLEFT) 
 	{
-		dmntchtWriteCheatProcessMemory(metadata.main_nso_extents.base + ADDR_AUDIOVOL1, &DefaultFloat1, sizeof(4));
+		util::Write8(0, ADDR_CheatBase, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool CarsOnWater2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool SportsCars2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xD, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xD, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool FlyingCars2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xE, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xE, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool BunnyHop2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xF, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0xF, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool DeathCar2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x12, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x12, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool SuperJump2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1A, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1A, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool InfBreath2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1C, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1C, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool WantedLock2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1F, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x1F, 0);
+		return true;		
+    }
+
+    return false;
+  });
+  return true;
+}
+
+template <typename T>
+bool SuperPunch2(T *entry) 
+{
+  entry->setClickListener([](u64 keys) 
+  { 
+    if (keys & KEY_DRIGHT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x21, 1);
+		return true;
+	}
+	else if (keys & KEY_DLEFT) 
+	{
+		util::Write8(0, ADDR_CheatBase + 0x21, 0);
 		return true;		
     }
 
