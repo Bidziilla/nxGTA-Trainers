0504823204d9c92af280c7e56e2c8d4565f0ff1c
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-12-19 01:50:08 -0500

    Update addresses.h
    Removed a bunch of addresses in favor of a 'base' offset for cheats and whatnot. For some reason, the cheats may toggle the wrong code. Oh well, fix it later cuz it's 1:50 AM at the time of writing this.

--- SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
+++ SanAndreas/source/CHEAT/DEFINITIONS/addresses.h
@@ -3,36 +3,16 @@
 #include <string>
 
 //PLAYER
-#define ADDR_CJMONEY 0x69B6B18              //MAIN
-
-//STATS
-#define ADDR_CJFAT 0x69B1EA8                //MAIN
-#define ADDR_CJSTAMINA 0x69B1EAC            //MAIN
-#define ADDR_CJMUSCLE 0x69B1EB0             //MAIN
-#define ADDR_CJMAXHEALTH 0x69B1EB4          //MAIN
-#define ADDR_CJRESPECT 0x69B1F54            //MAIN
+#define ADDR_CJMONEY 0x69CE618              //MAIN
 
 //TIME
-#define ADDR_TIME 0x698A264                 //MAIN
+#define ADDR_TIME 0x699F264                 //MAIN
 
 //PHYSICS
 #define ADDR_CJRUNSPEED 0xE13090            //MAIN
 
-//MISC
-#define ADDR_CHEATTRASHID 0xE96D84          //MAIN (A, R, A, R, Left, Left, R, L, A, Right)
-#define Addr_CarID 0x40
 
 //CHEATS     -   These are toggled as 0 = off, 1 = on. :3  All are in main
-#define ADDR_InsaneHandling 0x68D3F57
-#define ADDR_FlyingBoat 0x68D3F5E
-#define ADDR_CarsOnWater 0x68D3F5D
-#define ADDR_SportsCars 0x68D3F6B
-#define ADDR_BunnyHop 0x68D3F6D
-#define ADDR_FlyingCars 0x68D3F6C
-#define ADDR_DeathCar 0x68D3F70 
-#define ADDR_SuperJump 0x68D3F78            
-#define ADDR_InfBreath 0x68D3F7A
-#define ADDR_WantedLock 0x68D3F7D
-#define ADDR_SuperPunch 0x68D3F7F
+#define ADDR_CheatBase 0x68E8F5E //FlyingBoat
 
 #endif // ADDRESSES_H
