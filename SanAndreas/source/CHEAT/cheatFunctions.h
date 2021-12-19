ace7cfb9546444161cee87b543d8fe97de5ebfe7
Bidziilla <93905489+Bidziilla@users.noreply.github.com>
2021-12-19 01:48:17 -0500

    Update cheatFunctions.h
    Added new functions for writing to memory, it makes your code look a lot cleaner and you don't have to type those long ass lines anymore. Also added support for following pointers.

--- SanAndreas/source/CHEAT/cheatFunctions.h
+++ SanAndreas/source/CHEAT/cheatFunctions.h
@@ -6,13 +6,16 @@
 #ifndef CHEATFUNCTIONS_H
 #define CHEATFUNCTIONS_H
 
+uint64_t FollowPointer(uint64_t initAddr, const uint64_t* offsets, const uint32_t offsetsSize);
+
 namespace util
 {
 	u32 CopyHeapMemory(u32 read, u32 write);
 	u32 AddToOffset(u8 region, u32 Address, u32 Input);
 	u32 SubFromOffset(u8 region, u32 Address, u32 Input);
 	u32 ConvertToMOV(u32 Address, u8 Register, u16 Value);
-	//u32 EditTime(u8 Operation, u32 Address, u32 Input, u8 compare, u8 resetValue);
-	//u32 EnableCustomHud();
+	u32 Write8(u8 region, u32 Address, u8 Value);
+	u32 Write16(u8 region, u32 Address, u16 Value);
+	u32 Write32(u8 region, u32 Address, u32 Value);
 }
 #endif // CHEATFUNCTIONS_H
